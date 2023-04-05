f45eac0e7d6f24ccf4a8a021f54f056e5ea07ba3
Shashank Naik <shashanknaik808@gmail.com>
2023-04-06 00:52:03 +0530

    Most common progamming case types

--- 03/tut3.js
+++ 03/tut3.js
@@ -6,3 +6,40 @@ var channel;
 var marks = 0;
 channel = 'CodeWithJhonny'
 console.log(name, channel, marks);
+
+// Rules for creating JavaScript Variables
+/*
+1. Cannot start with numbers
+2. Can start with letter, numbers, _ or $
+3. Are case sensitive
+*/
+
+var city = 'Delhi';
+console.log(city);
+
+const ownersName = 'Hari Ram';
+// ownersName = 'Jhonny'; // Cannot do this (error)
+console.log(ownersName);
+const fruit = 'Orange';
+
+{
+    let city = 'Rampur';
+    city = 'Kolkota';
+    console.log(city);
+}
+console.log(city);
+
+const arr1 = [12,23,12,53,3];
+// arr1.push(45);
+onslotchange.log(arr1)
+
+/* Most common programming case types:
+
+1. camelCase
+2. kebab-case
+3. snake_case
+4. PascalCase
+
+*/
+
+

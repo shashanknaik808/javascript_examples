c7c1c8ae0bbf5d6746e51a03388f320bed8f777e
Shashank Naik <shashanknaik808@gmail.com>
2023-04-06 21:03:57 +0530

    04 Primitive data types (string, numbers, boolean, null, undefined)

---
+++ 04/tut4.js
@@ -0,0 +1,22 @@
+// Primitive data types
+
+//String
+let name = "shashank";
+console.log("My string is " + name);
+console.log("Data type is " + (typeof name));
+
+//Numbers
+let marks = 34.4;
+console.log("Data type is " + (typeof marks));
+
+//Boolean
+let isDriver = true;
+console.log("Data type is " + (typeof isDriver));
+
+// Null
+let nullVar = null;
+console.log("Data type is " + (typeof nullVar));
+
+// Undefined
+let undef = undefined;
+console.log("Data type is " + (typeof undef));

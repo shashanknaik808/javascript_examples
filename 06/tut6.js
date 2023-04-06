14f326b40c03cf4365dec4f84e048280a1517422
Shashank Naik <shashanknaik808@gmail.com>
2023-04-07 00:16:33 +0530

    06 Javascript template literals

--- 06/tut6.js
+++ 06/tut6.js
@@ -14,15 +14,23 @@ console.log(html);
 // console.log(html.toUpperCase());
 // console.log(html);
 
-console.log(html[1]);
-console.log(html.indexOf('<'));
-console.log(html.lastIndexOf('<'));
-console.log(html.charAt(3));
-console.log(html.endsWith('dsfsdfd'));
-console.log(html.includes(' fg'));
-console.log(html.substring(1,6));
+// console.log(html[1]);
+// console.log(html.indexOf('<'));
+// console.log(html.lastIndexOf('<'));
+// console.log(html.charAt(3));
+// console.log(html.endsWith('dsfsdfd'));
+// console.log(html.includes(' fg'));
+// console.log(html.substring(1,6));
 // console.log(html.slice(0, 4))
 // console.log(html.split('>'));
 // console.log(html.replace('this', 'it'));
+
+let fruit1 = 'Orange\'';
+let fruit2 = 'Apple';
+let myHtml = `Hello ${name}
+            <h1> This is "my" heading </h1>
+            <p> You like ${fruit1} and ${fruit2}`;
+
+document.body.innerHTML = myHtml;
 
 

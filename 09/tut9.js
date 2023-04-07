6cda3497fa93e0b78298d180bb0634f5660cb03c
Shashank Naik <shashanknaik808@gmail.com>
2023-04-08 00:56:53 +0530

    Do while loop in Js

--- 09/tut9.js
+++ 09/tut9.js
@@ -27,3 +27,26 @@ do {
   console.log(k + 1);
   k +=1;
 } while (k < 10);
+
+let arr = [2,5,6,4,2,3];
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element)
+// }
+
+arr.forEach(function(element, index, array){
+    console.log(element, index, array);
+});
+
+// let obj = {
+//     name: "Rohan Das",
+//     age: 78,
+//     type: "Dangerous Programmer",
+//     os: "Ubuntu"
+// }
+// for(let key in obj){
+//     console.log(`The ${key} of object is ${obj[key]}`)
+// }
+
+console.log('done');  

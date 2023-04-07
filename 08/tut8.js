6a69c5c62f85e2cbed72e52922f0229adce1b318
Shashank Naik <shashanknaik808@gmail.com>
2023-04-08 00:25:57 +0530

    08 Switches in Javascript

--- 08/tut8.js
+++ 08/tut8.js
@@ -3,31 +3,47 @@ const age = 128;
 const doesDrive = false;
 const vari = 34;
 
-if (age!=19){
+if (age != 19) {
     console.log('Age is not 19')
 }
 
-if (age !== 65){
+if (age !== 65) {
     console.log('Age is not 65')
 }
 
-else{
+else {
     console.log('Age is not 19')
 }
 
-if (typeof vari !== 'undefined'){
+if (typeof vari !== 'undefined') {
     console.log('Vari is defined');
 }
 
-else{
+else {
     console.log('Vari is not defined');
 }
 
-if (doesDrive || age>18){
+if (doesDrive || age > 18) {
     console.log("You can drive");
 }
-else{
+else {
     console.log("You cannot drive");
 }
 
-console.log(age==45? 'Age is 45': 'Age is not 45');
+console.log(age == 45 ? 'Age is 45' : 'Age is not 45');
+
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+
+    case 28:
+        console.log("You are 28");
+
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+}

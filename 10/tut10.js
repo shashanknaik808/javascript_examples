6da96151f4d79f1de7308568c0076ddc7af7910c
Shashank Naik <shashanknaik808@gmail.com>
2023-04-08 23:02:25 +0530

    10 Function expressions in Javascript

--- 10/tut10.js
+++ 10/tut10.js
@@ -15,8 +15,27 @@ function ui(name) {
 
 console.log(ui("shashank"), i)
 console.log(ui("shashank"), i)
-const mygreet = function(name, thank='Thank You'){
+const mygreet = function (name, thank = 'Thank You') {
     let msg = `Happy Birthday ${name} How I wish I could fly to you right now and be with you on this special day of yours. But remember, my good wishes are always there with you. ${thank}!`;
     return msg;
+}
 
-} 
+let name = 'SkillF';
+let name2 = 'Rohan';
+
+let val = mygreet(name, 'Thanks a lot');
+console.log(val);
+
+const myobj = {
+    name: "SkillF",
+    game: function () {
+        return "GTA";
+    }
+}
+console.log(myobj.game())
+
+arr = ['fruit', 'vegetable', 'furniture'];
+
+arr.forEach(function (element, index, array) {
+    console.log(element, index)
+});     

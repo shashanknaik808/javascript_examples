dea2a4bfbe6bf3c1fe8818fb0e0ebc5e2cf75ebd
Shashank Naik <shashanknaik808@gmail.com>
2023-04-08 22:16:46 +0530

    10 Function declaration in Javascript

---
+++ 10/tut10.js
@@ -0,0 +1,22 @@
+console.log('this is tut 10');
+
+if (1) {
+    let i = 234;
+    console.log(i)
+}
+
+console.log(i);
+
+function ui(name) {
+    let i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+}
+
+console.log(ui("shashank"), i)
+console.log(ui("shashank"), i)
+const mygreet = function(name, thank='Thank You'){
+    let msg = `Happy Birthday ${name} How I wish I could fly to you right now and be with you on this special day of yours. But remember, my good wishes are always there with you. ${thank}!`;
+    return msg;
+
+} 

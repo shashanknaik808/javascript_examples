92bbb9aa7798e040f09d5e35cbc6bcb3d60fd4ff
Shashank Naik <shashanknaik808@gmail.com>
2023-04-09 00:00:56 +0530

    12 DOM properties

--- 12/tut12.js
+++ 12/tut12.js
@@ -3,4 +3,9 @@ let a = document;
 a = document.all;
 a = document.body;
 a = document.forms[0];
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+a = document.links[0];
+// use document.images and document.scripts and print the list of images and scripts on an html page
 console.log(a);

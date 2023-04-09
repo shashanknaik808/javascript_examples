8b708f6d6a3398e4e6ed783e578b59b99a07acf9
Shashank Naik <shashanknaik808@gmail.com>
2023-04-09 23:43:57 +0530

    14 Multi element selector

--- 14/tut14.js
+++ 14/tut14.js
@@ -22,3 +22,20 @@ sel = document.querySelector('.child');
 sel = document.querySelector('div');
 sel.style.color = 'green';
 console.log(sel)
+
+// 2. Multi element selector
+let elems = document.getElementsByClassName('child');
+elems = document.getElementsByClassName('container');
+elems = document.getElementsByTagName('div');
+console.log(elems)
+
+for (let index = 0; index < elems.length; index++) {
+    const element = elems[index];
+    console.log(element);
+   element.style.color = 'blue';
+}
+// Array.from(elems).forEach(element => {
+//     console.log(element);
+//    element.style.color = 'blue'; 
+// });
+// console.log(elems[0].getElementsByClassName('child'))

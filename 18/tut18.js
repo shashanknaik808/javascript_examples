bfad77e0a2e93dccfddbccc8eba35259f6699903
Shashank Naik <shashanknaik808@gmail.com>
2023-04-11 00:19:32 +0530

    18 More on js events

---
+++ 18/tut18.js
@@ -0,0 +1,25 @@
+console.log('This is tut 18');
+let btn = document.getElementById('btn');
+btn.addEventListener('click', func1);
+btn.addEventListener('mousedown', func3);
+btn.addEventListener('dblclick', func2);
+
+function func1(e) {
+    console.log("Thanks", e);
+    e.preventDefault();
+}
+
+function func2(e) {
+    console.log("Thanks its a double click", e);
+    e.preventDefault();
+}
+
+function func3(e) {
+    console.log("Thanks its a mouse down ", e);
+    e.preventDefault();
+}
+document.querySelector('.no').addEventListener('mouseenter', function(){
+
+
+    console.log('You entered no')
+})

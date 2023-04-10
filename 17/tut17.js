6ee0e11253c951be9188bc22efc3e78707793ece
Shashank Naik <shashanknaik808@gmail.com>
2023-04-10 22:34:40 +0530

    17 Event properties

--- 17/tut17.js
+++ 17/tut17.js
@@ -6,4 +6,12 @@ document.getElementById("heading").addEventListener("click", function(e){
     variable = e.target;
     variable = e.target.className;
     variable = Array.from(e.target.classList);
+
+    variable = e.target.id;
+    variable = e.offsetX;
+    variable = e.offsetY;
+    variable = e.clientX;
+    variable = e.clientY;
+    console.log(variable);
+    // location.href = '//codewithharry.com'
 });

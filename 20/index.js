15aac7b65afb52301a43bf7c9d49f41eecd11220
Shashank Naik <shashanknaik808@gmail.com>
2023-04-11 21:23:14 +0530

    20 Local storage in Js

---
+++ 20/index.js
@@ -0,0 +1,10 @@
+console.log('This is tut 20');
+let impArray = ['adrak', 'pyaz', 'bhindi'];
+
+// Add a key-value pair inside local Storage
+// localStorage.setItem('Name', 'Harry');
+// localStorage.setItem('Name2', 'Rohan');
+// localStorage.setItem('Sabzi', JSON.stringify(impArray));
+
+// Clears the entire local storage
+// localStorage.clear();

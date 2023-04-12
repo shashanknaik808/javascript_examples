d5377d14102562197fe930fcced745808cf509f3
Shashank Naik <shashanknaik808@gmail.com>
2023-04-13 00:10:41 +0530

    23 Exploring the Math object

---
+++ 23/index.js
@@ -0,0 +1,28 @@
+console.log("Welcome to tutorial number 23");
+let x = 3;
+let y = 6;
+let z;
+z = x+y;
+z = x-y;
+z = x*y;
+z = x/y;
+
+// Exploringg the Math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.round(5.4);
+z = Math.ceil(5.3);
+z = Math.floor(-5.3);
+z = Math.abs(5);
+console.log(z);
+
+// 3
+// 2.8
+// 2
+// 1
+// 0
+// -1
+// -2
+// -2.7
+// -3

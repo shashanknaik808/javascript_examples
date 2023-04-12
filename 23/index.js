aa828a754877f1a5463d5d38a86cfa620fc154f9
Shashank Naik <shashanknaik808@gmail.com>
2023-04-13 00:18:20 +0530

    23 Math object methods

--- 23/index.js
+++ 23/index.js
@@ -15,6 +15,17 @@ z = Math.round(5.4);
 z = Math.ceil(5.3);
 z = Math.floor(-5.3);
 z = Math.abs(5);
+z = Math.sqrt(64);
+z = Math.pow(2, 3);
+z = Math.min(2, 3, 34,234, 2342,34);
+z = Math.max(2, 3, 34,234,34);
+z = Math.random();
+z = 100*Math.random();
+z = Math.ceil(50 + (100-500)*Math.random());
+// a = (0, 1)
+// a100 = a*100 = (0, 100)
+// a10_100 = 10+a*(100 - 10)
+
 console.log(z);
 
 // 3

32f39f55748ffea7613a32a89626e87de6b6bb8c
Shashank Naik <shashanknaik808@gmail.com>
2023-04-13 22:15:30 +0530

    25 Added eventListener to divElem

--- 25/index.js
+++ 25/index.js
@@ -6,29 +6,44 @@ whenever someone clicks on the div, it should be converted into an editable item
 */
 
 // Create a new element
-let divElm = document.createElement('div');
+let divElem = document.createElement('div');
 
 // Add text to that created element
 let val = localStorage.getItem('text');
 let text;
-if (val==null){
-    text = document.createTextNode('This is my elent. click to edit it');   
+if (val == null) {
+    text = document.createTextNode('This is my elent. click to edit it');
 }
-else{
+else {
     text = document.createTextNode(val);
 }
-divElm.appendChild(text);
+divElem.appendChild(text);
 
 // Give element id, style and class
-divElm.setAttribute('id', 'elm');
-divElm.setAttribute('class', 'elm');
-divElm.setAttribute('style', 'border:2px solid black; width: 154px; margin: 34px; padding:23px;');
+divElem.setAttribute('id', 'elm');
+divElem.setAttribute('class', 'elm');
+divElem.setAttribute('style', 'border:2px solid black; width: 154px; margin: 34px; padding:23px;');
 
 // Grab the main container
 let container = document.querySelector('.container');
 let first = document.getElementById('myfirst');
 
 // Insert the element before element with id first
-container.insertBefore(divElm, first);
+container.insertBefore(divElem, first);
 
-console.log(divElm, container, first);
+console.log(divElem, container, first);
+
+// add event listener to the divElem
+divElem.addEventListener('click', function () {
+    let noTextAreas = document.getElementsByClassName('textarea').length;
+    if (noTextAreas == 0) {
+        let html = elem.innerHTML;
+        divElem.innerHTML = ` <textarea class="textarea form-control" id="textarea" rows="3"> ${html}</textarea>`;
+    }
+    //listen for the blur event on textarea
+    let textarea = document.getElementById('textarea');
+    textarea.addEventListener('blur', function () {
+        elem.innerHTML = textarea.value;
+        localStorage.setItem('text', textarea.value);
+    })
+});

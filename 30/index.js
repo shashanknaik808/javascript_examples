b4eedc2feb5fa35c8e5ff99d7f215fe6821f0a75
Shashank Naik <shashanknaik808@gmail.com>
2023-04-14 23:13:25 +0530

    30 Constructor creation

--- 30/index.js
+++ 30/index.js
@@ -14,7 +14,7 @@ let harry = Object.create(proto);
 harry.name = "harry";
 harry.role = "Programmer";
 // harry.changeName("Harry2")
-console.log(harry)
+// console.log(harry)
 
 // This also creates harry object
 const harry1 = Object.create(proto, {
@@ -22,4 +22,26 @@ const harry1 = Object.create(proto, {
     role: { value: "Programmer" },
 });
 harry1.changeName("Harry2")
-console.log(harry1)
+// console.log(harry1)
+
+// Employee constructor
+function Employee(name, salary, experience) {
+    this.name = name;
+    this.salary = salary;
+    this.experience = experience;
+}
+
+// Slogan
+Employee.prototype.slogan = function () {
+    return `This company is the best. Regards, ${this.name}`;
+}
+
+// Create an object from this constructor now
+let harryObj = new Employee("Harry", 345099, 87);
+console.log(harryObj.slogan())
+
+// Programmer
+function Programmer(name, salary, experience, language) {
+    Employee.call(this, name, salary, experience);
+    this.language = language;
+}

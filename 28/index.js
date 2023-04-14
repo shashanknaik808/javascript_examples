abe25f8f0742deb31ee4ef867cb6e435323dc6a3
Shashank Naik <shashanknaik808@gmail.com>
2023-04-14 21:17:17 +0530

    28 Object literals

---
+++ 28/index.js
@@ -0,0 +1,12 @@
+console.log("This is tutorial 28");
+
+// Object literal : Object.prototype
+let obj = {
+    name: "harry",
+    channel: "Code With Harry",
+    address: "Mars"
+}
+
+function Obj(givenName){
+    this.name = givenName
+}

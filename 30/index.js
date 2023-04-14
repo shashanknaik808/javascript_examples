78f20b19fbdd8ffd45cd2fff72df01f6258463bb
Shashank Naik <shashanknaik808@gmail.com>
2023-04-14 23:28:03 +0530

    30 Prototype inheritance

--- 30/index.js
+++ 30/index.js
@@ -45,3 +45,12 @@ function Programmer(name, salary, experience, language) {
     Employee.call(this, name, salary, experience);
     this.language = language;
 }
+
+// Inherit the prototype
+Programmer.prototype = Object.create(Employee.prototype);
+
+// Manually set the constructor
+Programmer.prototype.constructor = Programmer;
+
+let rohan = new Programmer("Rohan", 2, 0, "Javascript");
+console.log(rohan);

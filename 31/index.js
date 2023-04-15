59a08c0de41b5047c6d7c60495cadca4a86be26c
Shashank Naik <shashanknaik808@gmail.com>
2023-04-16 00:12:08 +0530

    31 Extending the class

--- 31/index.js
+++ 31/index.js
@@ -7,7 +7,32 @@ class Employee {
         this.division = givenDivision;
     }
 
-    slogan(){
+    slogan() {
         return `I am ${this.name} and this company is the best`;
+    }
+
+    joiningYear() {
+        return 2020 - this.givenExperience;
+    }
+
+    static add(a, b) {
+        return a + b;
+    }
+}
+
+class Programmer extends Employee {
+    constructor(givenName, givenExperience, givenDivision, language, github) {
+        super(givenName, givenExperience, givenDivision);
+        this.language = language;
+        this.github = github;
+    }
+
+    favouriteLanguage() {
+        if (this.language == 'python'){
+            return 'Python';
+        }
+        else{
+            return 'JavaScript';
+        }
     }
 }

2c29287a50c88523806e7dd9ce851f0cefea980d
Shashank Naik <shashanknaik808@gmail.com>
2023-04-17 13:03:15 +0530

    Basics of JavaScript 1st commit

---
+++ 0A/index.js
@@ -0,0 +1,76 @@
+/*console.log("Hello World");
+console.log('My name is Shashank');
+console.log(1987);
+console.log(true);
+console.log(false);*/
+// console.log("Hello World"); // Single line comment
+
+// There are 6 primary datatypes in javascript
+// a) Number b)String c)Boolean d)Null e)Undefined f)Object
+
+/*console.log(5); //Number datatype
+console.log(25.69);
+
+console.log('Javascript'); // String
+console.log('My name is Shashank'); // String
+
+console.log(5 > 6); // Result is Boolean
+console.log(5 <= 25); */
+
+/*
+var a = null; //Asigning Null
+console.log(a); // Output is Null
+
+var b; // Not definig anything
+console.log(b); //Output is undefined
+*/
+
+/*
+console.log(76 + 68); // Addition
+console.log(45 - 34); //Substraction
+console.log(65 * 55); // Multiplication
+console.log(65 / 55); // Division-Quotient
+console.log(65 % 55); // Modulo - Remainder 
+*/
+
+/*
+//String Concatenation
+console.log('Hello' + 'World'); //HelloWorld
+console.log('Hello' + ' ' + 'World'); //Hello World
+
+//String length
+console.log('Hello'.length); //5
+console.log('My name is Anthony Gonsalves'.length); //28
+*/
+
+/*
+console.log('Hello'.toUpperCase());  //HELLO
+console.log('VaNaKKam'.toLowerCase()); //vanakkam
+console.log('Super Ranga'.startsWith('S')); // true
+console.log('Super Ranga'.startsWith('R')); // false
+console.log('        No Space Please                '.trim()); 
+*/
+
+/*
+// Built-in Objects
+console.log(Math.random()); // Generates random number b/w 0 to 1
+console.log(Math.floor(46.7878)); // 46
+console.log(Math.ceil(46.7878)); //47
+console.log(Number.isInteger(45)); //true
+console.log(Number.isInteger(45.666)); //false 
+*/
+
+// Generate a random num. b/w 0 to 75.
+// console.log(Math.ceil(Math.random() * 75));
+
+/*
+// Generate a dice programm b/w 1 to 6
+let num = Math.random() * 7;
+let rou = Math.floor(num);
+if (rou === 0) {
+    console.log("try again");
+}
+else {
+    console.log(rou);
+}
+*/
